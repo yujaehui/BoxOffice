e0bf63db1b14e6dac92ab830d82e30f2743073ce
Jaehui Yu <pkj0522@naver.com>
2024-04-08 15:03:20 +0900

    Initial Commit

---
+++ BoxOffice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoxOffice
+//
+//  Created by Jaehui Yu on 4/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
